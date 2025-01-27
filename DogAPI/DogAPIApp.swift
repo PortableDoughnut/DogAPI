f5b98073ca2709bd4a4d51ed55855b3378b51bef
PortableDoughnut <gwen.thelin@gmail.com>
2025-01-27 09:10:19 -0700

    Initial Commit

---
+++ DogAPI/DogAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogAPIApp.swift
+//  DogAPI
+//
+//  Created by Gwen Thelin on 1/27/25.
+//
+
+import SwiftUI
+
+@main
+struct DogAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
